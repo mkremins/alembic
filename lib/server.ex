808dc3b29d32004b200ae9eabeef261c71027066
Max Kreminski <maxkreminski@gmail.com>
2013-09-02 13:46:33 -0700

    Document all Alembic.Server functionality

--- lib/server.ex
+++ lib/server.ex
@@ -1,6 +1,10 @@
 defmodule Alembic.Server do
 	use GenServer.Behaviour
 
+	@doc """
+	Starts the TCP server and spawns an acceptor process listening for incoming
+	connections on the specified hostname and port.
+	"""
 	def init({host, port}) do
 		listening = Socket.TCP.listen(local: [address: host, port: port])
 		spawn fn ->
@@ -8,12 +12,21 @@ defmodule Alembic.Server do
 		end
 	end
 
+	@doc """
+	Handles a new connection by spawning a new client process and directing all
+	future incoming traffic on the specified socket to that process.
+	"""
 	def handle_cast({:connect, socket}, state) do
 		client_pid = Alembic.ClientSupervisor.spawn_client(socket)
 		Socket.TCP.process(socket, client_pid)
 		{:noreply, state}
 	end
 
+	@doc """
+	Listens for a new incoming connection. When a client successfully connects,
+	creates a new socket object for communication with that client and notifies
+	the main server process of the existence of the new connection.
+	"""
 	defp acceptor(listening, pid) do
 		case Socket.TCP.accept!(listening) do
 			{:ok, socket} ->
