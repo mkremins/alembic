4304217007bee05b4ea0214fa44fb6cad2c2c8f8
Max Kreminski <maxkreminski@gmail.com>
2013-09-05 15:34:58 -0700

    Remove generic comments from Mixfile

--- mix.exs
+++ mix.exs
@@ -8,13 +8,10 @@ defmodule Alembic.Mixfile do
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     [ mod: {Alembic, []} ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [ {:exactor, "0.1",       [github: "sasa1977/exactor"]},
       {:socket,  "0.2.0-dev", [github: "meh/elixir-socket"]} ]
