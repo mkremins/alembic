56ca22c16df58f3c78e4d3d4d06d95d4454027b6
Max Kreminski <maxkreminski@gmail.com>
2013-09-07 10:39:25 -0700

    Clean up Minecraft interop code
    Rather than attempting to anticipate and handle every
    potential source of errors on the spot, we're instead
    just going to wrap the potentially problematic functions
    (essentially anything that reads from a socket) in a
    single try/rescue block at the 'Protocol.read_request/1'
    level (the primary external entry point for the entire
    Alembic.Minecraft module namespace) and let the internals
    fail at will.
    
    We're also going to introduce a new 'defparser/2' macro
    that can be used to define parser functions that take a
    packet payload of a particular type and return a request
    that the Alembic core plugin API can understand, thereby
    allowing us to separate the specification of data types
    and intended interpretation for each packet's payload.

--- lib/minecraft/macros.ex
+++ lib/minecraft/macros.ex
@@ -7,16 +7,34 @@ defmodule Alembic.Minecraft.Macros do
 
 	@doc """
 	Defines a packet handler function for the specified packet ID that will
-	read each of the specified types from the socket in order.
+	read each of the specified types from the socket in order. The handler
+	function will return a keyword list mapping the names specified in `types`
+	to the actual values that were read from the socket, raising an exception
+	in the event that something goes wrong.
 	"""
 	defmacro defpacket(id, types) do
 		quote do
-			def read_packet(unquote(id), socket) do
-				packet = Keyword.new
+			def read_payload!(unquote(id), socket) do
+				payload = Keyword.new
 				lc {key, type} inlist unquote(types) do
-					Keyword.put(packet, key, Protocol.read(type, socket))
+					Keyword.put(payload, key, Protocol.read!(type, socket))
 				end
-				_return = packet
+				_return = {:ok, payload}
+			end
+		end
+	end
+
+	@doc """
+	Defines a parser function for the specified packet ID that will parse the
+	payload of a packet with that ID and return a request object to be handled
+	by plugins. The parser function will receive as its `payload` argument the
+	keyword list returned by the `read_payload!/2` clause corresponding to the
+	specified packet ID.
+	"""
+	defmacro defparser(id, code) do
+		quote do
+			def parse_request(unquote(id), payload) do
+				unquote(code)
 			end
 		end
 	end
--- lib/minecraft/protocol.ex
+++ lib/minecraft/protocol.ex
@@ -7,68 +7,56 @@ defmodule Alembic.Minecraft.Protocol do
 	alias Alembic.Minecraft.Packets
 
 	@doc """
-	Reads the next byte to come over the socket, assumes it's a packet ID, and
-	then delegates reading to the `read_packet/2` function clause corresponding
-	to that ID.
+	Reads the next byte to come over the socket; assumes that byte is a packet
+	ID; uses the corresponding `Alembic.Minecraft.Packets.read_payload!/2`
+	clause to read the packet's payload; and translates the ID/payload pair
+	(essentially, the entire packet) into a request that can be handled by
+	the server's plugins.
 
-	Returns `{:ok, request}` in the event of a successful packet read,
+	Returns `{:ok, request}` in the event that a request is successfully read,
 	`{:error, reason}` in the event of a failure.
 	"""
-	def read_packet(socket) do
-		case read(:byte, socket) do
-			{:error, reason} ->
-				{:error, reason}
-			packet_id -> 
-				case Packets.read_packet(packet_id, socket) do
-					{:error, reason} ->
-						{:error, reason}
-					request ->
-						{:ok, request}
-				end
+	def read_request(socket) do
+		try do
+			packet_id = read!(:byte, socket)
+			{:ok, payload} = Packets.read_payload!(packet_id, socket)
+			_return = {:ok, Packets.parse_request(id, payload)}
+		rescue
+			e in [RuntimeError] -> {:error, e.message}
 		end
 	end
 
-	####################################
-	# generic data type definitions
-	####################################
-
 	@doc """
+	Reads a value of the specified type from the specified socket. Returns the
+	value that was read, raising an exception if something went wrong.
+
 	Strings, unlike other data types that we need to read, are of indeterminate
-	length. Therefore, we use a special `read/2` clause to handle strings and
+	length. Therefore, we use a special `read!/2` clause to handle strings and
 	strings alone.
 	"""
-	defp read(:string, socket) do
-		case read(:short, socket) do
+	defp read!(:string, socket) do
+		case read!(:short, socket) do
 			0 ->
 				""
-			length when is_integer(length) ->
-				case socket.recv(length * 2) do
-					{:ok, bitstring} ->
-						bitstring # TODO: convert to utf-16
-					error ->
-						error
-				end
-			error ->
-				error
+			length ->
+				{:ok, bitstring} = socket.recv!(length * 2) do
+				bitstring # TODO: convert to utf-16
 		end
 	end
 
 	@doc """
-	Reads a value of the specified type from the specified socket.
+	Reads a value of the specified type from the specified socket. Returns the
+	value that was read, raising an exception if something went wrong.
 	"""
-	defp read(type, socket) do
+	defp read!(type, socket) do
 		bytes = byte_length(type)
-		case socket.recv(bytes) do
-			{:ok, bitstring} ->
-				format(type, bitstring)
-			{:error, reason} ->
-				{:error, reason} # TODO: raise exception
-		end
+		{:ok, bitstring} = socket.recv!(bytes)
+		format!(type, bitstring)
 	end
 
 	@doc """
 	Returns the length, in bytes, of a value of the specified type. Used by
-	`read/2` to determine how many bytes should be read from a socket before
+	`read!/2` to determine how many bytes should be read from a socket before
 	attempting to translate those bytes into a value.
 	"""
 	defp byte_length(type) do
@@ -85,9 +73,10 @@ defmodule Alembic.Minecraft.Protocol do
 
 	@doc """
 	Matches the bitstring against a pattern associated with the specified type,
-	extracting and returning a value of that type.
+	extracting and returning a value of that type. If the match fails, an
+	exception will be raised.
 	"""
-	defp format(type, bitstring) do
+	defp format!(type, bitstring) do
 		s = byte_length(type) * 8
 		# Seems like there's probably a better way to do this.
 		# Maybe use macros or something?
