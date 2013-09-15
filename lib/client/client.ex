8a2a47157642cbaa0ebbf7ac2dc80c947b3ad9dd
Max Kreminski <maxkreminski@gmail.com>
2013-09-15 11:14:46 -0700

    Refactor Client module for generality
    We're finally able to remove all specific references to the
    Minecraft protocol translator module from the generic Client
    module; the Client module, like many of the other modules we've
    recently refactored, is now protocol-agnostic.
    
    I've also taken this opportunity to clean up some of the ExActor
    code in the Client module, increasing the readability of said
    code by a good amount.

--- lib/client/client.ex
+++ lib/client/client.ex
@@ -1,7 +1,7 @@
 defmodule Alembic.Client do
 	@moduledoc """
 	Represents a client currently connected to the server. Responsible for
-	reading packets from, and writing packets to, the socket associated with
+	reading requests from, and writing events to, the socket associated with
 	that client.
 	"""
 
@@ -14,25 +14,26 @@ defmodule Alembic.Client do
 	socket and setting up a new State record to hold information about the
 	connected client.
 	"""
-	definit socket do
-		client_pid = self
+	definit {socket, translator} do
+		client = self
 		reader = spawn fn ->
-			reader(client_pid, socket)
+			reader(client, socket, translator)
 		end
 		Socket.TCP.process(socket, reader)
-		initial_state(State[socket: socket])
+		State[socket: socket]
 	end
 
 	@doc """
-	Called when the reader process successfully reads a packet from the socket
-	and interprets the payload as a valid request.
+	Called when the reader process successfully reads a request from the
+	connected client.
 	"""
 	defcast request(request), state: state do
 		# TODO
 	end
 
 	@doc """
-	Called when an attempt to read from the socket returns an error.
+	Called when the reader process encounters an error while attempting to read
+	a request.
 	"""
 	defcast error(reason) do
 		# TODO handle the error (maybe log it?)
@@ -40,28 +41,28 @@ defmodule Alembic.Client do
 
 	@doc """
 	Called when the client disconnects, either gracefully (by sending a
-	disconnect packet over the socket) or forcibly (when reading from the
-	socket errors out).
+	disconnect request over the socket) or forcibly (when an attempt to read
+	from the socket errors out).
 	"""
 	defcast disconnect do
 		# TODO
 	end
 
 	@doc """
-	Attempts to read a packet from the specified socket, then passes the packet
-	to the specified client process as a message and resumes reading (if a
-	packet was successfully read) or passes an error message to the client
-	process and shuts down (if something went wrong).
+	Attempts to read a request from the specified socket using the specified
+	protocol translator. If a request is successfully read, that request is
+	then passed to the specified client process as a message; if something goes
+	wrong, an error message is passed to the client process instead.
 	"""
-	defp reader(client, socket) do
-		case Alembic.Minecraft.Protocol.read_request(socket) do
+	defp reader(client, socket, translator) do
+		case translator.read_request(socket) do
 			{:ok, request} ->
-				client.request(request)
-				reader(client, socket)
+				request(client, request)
+				reader(client, socket, translator)
 			{:error, :closed} ->
-				client.disconnect
+				disconnect(client)
 			{:error, reason} ->
-				client.error(reason)
+				error(client, reason)
 		end
 	end
 end
