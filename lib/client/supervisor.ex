7cb055e892732897fa3e4425636453e19c326469
Max Kreminski <maxkreminski@gmail.com>
2013-09-18 19:30:09 -0700

    Add documentation to ClientSupervisor module

--- lib/client/supervisor.ex
+++ lib/client/supervisor.ex
@@ -6,11 +6,21 @@ defmodule Alembic.ClientSupervisor do
 		:supervisor.start_link(__MODULE__, [])
 	end
 
+	@doc """
+	Initializes the client supervisor, defining the child specification for an
+	individual client process and tweaking the supervisor's own supervisor
+	specification for optimal performance with dynamic children.
+	"""
 	def init(args) do
 		tree = [worker(Alembic.Client, args, modules: :dynamic)]
 		supervise(tree, strategy: :simple_one_for_one)
 	end
 
+	@doc """
+	Spawns and begins supervising a new client process, using the translator
+	module with the specified name to translate packets and using the specified
+	socket to communicate with the connected client.
+	"""
 	def spawn_client(socket, type) do
 		translator = Module.concat(Alembic.Client, type)
 		case Code.ensure_loaded(translator) do
